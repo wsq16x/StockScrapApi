c7d763b11e4cd57487a81cf34338d7347612b813
wsq16x <53550150+wsq16x@users.noreply.github.com>
2023-04-01 00:50:08 +0600

    Added fallback endpoints for fetching imaged from Firebase

--- StockScrapApi/Controllers/ScraperController.cs
+++ StockScrapApi/Controllers/ScraperController.cs
@@ -1,6 +1,7 @@
 ﻿using Hangfire;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StockScrapApi.Helpers;
 using StockScrapApi.Scraper;
 
 namespace StockScrapApi.Controllers
@@ -11,17 +12,35 @@ namespace StockScrapApi.Controllers
     {
         private readonly IScraper _scraper;
         private readonly IBackgroundJobClient _backgroundJobClient;
+        private readonly IMapFirebaseData _mapFirebaseData;
 
-        public ScraperController(IScraper scraper, IBackgroundJobClient backgroundJobClient)
+        public ScraperController(IScraper scraper, IBackgroundJobClient backgroundJobClient, IMapFirebaseData mapFirebaseData)
         {
             _scraper = scraper;
             _backgroundJobClient = backgroundJobClient;
+            _mapFirebaseData = mapFirebaseData;
         }
 
         [HttpPost]
         public async Task<IActionResult> RunScraper()
         {
             var JobId = _backgroundJobClient.Enqueue(() => _scraper.ScrapeAndPush(false, true));
+            return Ok(string.Format("Job Created with Id {0}", JobId));
+        }
+
+        [Route("/FetchCompanyLogos")]
+        [HttpPost]
+        public async Task<IActionResult> FetchCompanyLogos()
+        {
+            var JobId = _backgroundJobClient.Enqueue(() => _mapFirebaseData.GetCompanyLogo());
+            return Ok(string.Format("Job Created with Id {0}", JobId));
+        }
+
+        [Route("/FetchPersonPhotos")]
+        [HttpPost]
+        public async Task<IActionResult> FetchPersonPhotos()
+        {
+            var JobId = _backgroundJobClient.Enqueue(() => _mapFirebaseData.GetProfilePictures());
             return Ok(string.Format("Job Created with Id {0}", JobId));
         }
     }
