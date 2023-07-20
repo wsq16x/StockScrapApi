142cf2a1965738799697b39e02a3aaf5eae5ecb6
wsq16x <53550150+wsq16x@users.noreply.github.com>
2023-07-20 12:01:09 +0600

    Trim BasicInfo

--- StockScrapApi/Scraper/ScrapeData.cs
+++ StockScrapApi/Scraper/ScrapeData.cs
@@ -65,14 +65,14 @@ namespace StockScrapApi.Scraper
             double valF;
             int valI;
 
-            var authorizedCapital = allTables.Nodes[2].SelectSingleNode("./tr[1]/td[1]").InnerText;
-            var debutTradingDate = allTables.Nodes[2].SelectSingleNode("./tr[1]/td[2]").InnerText;
-            var instrumentType = allTables.Nodes[2].SelectSingleNode("./tr[2]/td[2]").InnerText;
-            var faceParValue = allTables.Nodes[2].SelectSingleNode("./tr[3]/td[1]").InnerText;
-            var paidUpCapital = allTables.Nodes[2].SelectSingleNode("./tr[2]/td[1]").InnerText;
-            var marketLot = allTables.Nodes[2].SelectSingleNode("./tr[3]/td[2]").InnerText;
-            var totalOutstandingSecurity = allTables.Nodes[2].SelectSingleNode("./tr[4]/td[1]").InnerText;
-            var sector = allTables.Nodes[2].SelectSingleNode("./tr[4]/td[2]").InnerText;
+            var authorizedCapital = allTables.Nodes[2].SelectSingleNode("./tr[1]/td[1]").InnerText.Trim();
+            var debutTradingDate = allTables.Nodes[2].SelectSingleNode("./tr[1]/td[2]").InnerText.Trim();
+            var instrumentType = allTables.Nodes[2].SelectSingleNode("./tr[2]/td[2]").InnerText.Trim();
+            var faceParValue = allTables.Nodes[2].SelectSingleNode("./tr[3]/td[1]").InnerText.Trim();
+            var paidUpCapital = allTables.Nodes[2].SelectSingleNode("./tr[2]/td[1]").InnerText.Trim();
+            var marketLot = allTables.Nodes[2].SelectSingleNode("./tr[3]/td[2]").InnerText.Trim();
+            var totalOutstandingSecurity = allTables.Nodes[2].SelectSingleNode("./tr[4]/td[1]").InnerText.Trim();
+            var sector = allTables.Nodes[2].SelectSingleNode("./tr[4]/td[2]").InnerText.Trim();
 
             var basicInfo = new BasicInfo
             {
